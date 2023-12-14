4a46358a43c0929582a7562d1e57f5bc0e452183
SMANI2887 <smani2887@gmail.com>
2023-12-15 05:08:01 +0530

    NodeJS Tast-1 FileSystem

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require ('express');
+const fs = require ("fs");
+const  { type } = require ("os");
+
+const app = express();
+
+const d = new Date();
+const time = d.toLocaleTimeString();
+const realTime = time.slice(0,5).split(':').join('_');
+console.log(realTime);
+
+const date = d.toLocaleDateString();
+const realDate = date.split('/').join('_');
+
+
+const fileTitle = realDate  + '-' + realTime;
+const newFileTitle = fileTitle.split(' ').join('');
+console.log(newFileTitle);
+
+
+app.get('/',(request, response) => {
+    
+    // first question solution
+    fs.writeFile(`./newfiles/${newFileTitle}.txt`, newFileTitle, (err) => {
+            if(err) console.log(err);
+            console.log("File created.");
+        });
+    
+
+    // second question answer
+    fs.readdir('./newfiles', (err, files) => {
+    if(err) console.log(err);
+    files.forEach((file) => {
+        console.log(file);
+
+        response.writeHead(200,{'Content-Type' : 'text/plain'});
+        response.end(JSON.stringify(file)); 
+    })
+    
+})
+})
+
+const PORT=3001;
+app.listen(PORT);
+console.log(`Server is running ${PORT}`);
